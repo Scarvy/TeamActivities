b69f476a12f57de9a9d3965e772490f03ad6c3b5
Scott Carvalho <scottcarvalho71@gmail.com>
2024-06-10 10:44:46 -0400

    ta6: working on team activity 6
    Co-authored-by: Abegail Santos <asnts18@users.noreply.github.com>
    Co-authored-by: Tracy Urquhart <tracy-u@users.noreply.github.com>
    Co-authored-by: JennyjieyiZ <JennyjieyiZ@users.noreply.github.com>
    Co-authored-by: Maricarmen Rodriguez <maricarmencodes@users.noreply.github.com>

--- Module06/Flight.java
+++ Module06/Flight.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.TreeSet;
+import java.util.TreeMap;
+import java.util.Set;
 
 public class Flight implements Comparable<Flight> {
     private String airline;
     private int flightNumber;
     private String airlineCode;
 
-    public Flight(String airline, int flightNumber, String airlineCode) {
+    public Flight(String airline, int flightNumber, String airlineCode) { 
         this.airline = airline;
         this.flightNumber = flightNumber;
         this.airlineCode = airlineCode;
@@ -21,6 +23,9 @@ public class Flight implements Comparable<Flight> {
         return airlineCode + flightNumber;
     }
 
+    public String getAirlineCode() {
+        return airlineCode;
+    }
 
     @Override
     public int compareTo(Flight o) {
@@ -31,6 +36,30 @@ public class Flight implements Comparable<Flight> {
     public String toString() {
         return "Flight{" + "airline='" + airline + '\'' + ", flightNumber=" + flightNumber
                 + ", airlineCode='" + airlineCode + '\'' + '}';
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this==other) {
+            return true;
+        }
+
+        if (!(other instanceof Flight f)) {
+            return false;
+        }
+
+        return (
+            (this.airline.equals(f.airline)) && 
+            (this.airlineCode.equals(f.airlineCode)) && 
+            (this.flightNumber == f.flightNumber)
+        );
+
+    }
+
+    
+    @Override
+    public int hashCode() {
+        return getFlightID().hashCode();
     }
 
 
@@ -46,24 +75,45 @@ public class Flight implements Comparable<Flight> {
         List<Flight> flights = new ArrayList<>();
         // can't use list.of() only, as it returns an immutable list, so we add one to new list
         flights.addAll(List.of(f1, f2, f3));
-
+        System.out.println(); // for formatting
         System.out.println("preSorted");
         System.out.println(flights);
 
         flights.sort(null); //if null is passed as parameter, it will sort the lst in place using the compareTo        
+        System.out.println();
         System.out.println("postSorted");
         System.out.println(flights);
 
-        // TODO: Create a lambda expression to sort the flights by flight number
+        // Create a lambda expression to sort the flights by flight number
+        System.out.println();
+        System.out.println("Printing using lambda expressions, sorting by flight number");
+        flights.sort((flight1, flight2) -> flight1.getFlightNumber() - flight2.getFlightNumber());
+        System.out.println(flights);
 
+        // Create a lambda expression to sort the flights by flight code
+        System.out.println();
+        System.out.println("Printing using lambda expressions, sorting by flight code");
+        flights.sort((flight1, flight2) -> flight1.getAirlineCode().compareTo(flight2.getAirlineCode()));
         System.out.println(flights);
 
 
-        // TODO: Add code using a TreeSet to store the flights in order
+        // Add code using a TreeSet to store the flights in order
+        System.out.println();
+        System.out.println("Adding a TreeSet");
+        Set<Flight> flightSet = new TreeSet<>(flights);
+        System.out.println(flightSet);
 
-
-
-        // TODO: Add code to store the flights in a TreeMap
+        // Add code to store the flights in a TreeMap
         // use the flight as key, and Integer as value - storing the number of seats on the flight
+        System.out.println();
+        System.out.println("Adding a TreeMap");
+        TreeMap<Flight, Integer> flightSeats = new TreeMap<>();
+
+        // assuming F1, F2, F3 are already created
+        flightSeats.put(f1, 100);
+        flightSeats.put(f2, 200);
+        flightSeats.put(f3, 300);
+
+        System.out.println(flightSeats);
     }
 }
